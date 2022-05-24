2126d1f8da2b5cf3764bf23fc6d8ff192afb31cf
marie <dul.sreyneang@gmail.com>
2022-05-24 10:16:00 +0800

    Create an @IBOutlet for the email text field. Create an @IBAction for the submit button.

--- Contest/ViewController.swift
+++ Contest/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func submitButtonPress(_ sender: UIButton) {
+    }
+    
 
 
 }
