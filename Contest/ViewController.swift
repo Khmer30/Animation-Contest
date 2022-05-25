9952ddf6427a469e75486a38b51774165c592657
marie <dul.sreyneang@gmail.com>
2022-05-25 18:01:16 +0800

    Add textFiled animation
    -if text-filed is empty show animation
    -else text-filed has value perform segue

--- Contest/ViewController.swift
+++ Contest/ViewController.swift
@@ -7,20 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
-    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var textField: UITextField!
+    let emailTextField = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        textField.delegate = self
     }
     
     @IBAction func submitButtonPress(_ sender: UIButton) {
+        
+        textField.resignFirstResponder()
+        if textField.text == emailTextField {
+            UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+                self.textField.center.x += 10
+            }, completion: nil)
+            
+            UIView.animate(withDuration: 0.1, delay: 0.1, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+                self.textField.center.x -= 20
+            }, completion: nil)
+            
+            UIView.animate(withDuration: 0.1, delay: 0.1, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+                self.textField.center.x += 10
+            }, completion: nil)
+
+        } else {
+            textField.text = ""
+
+            performSegue(withIdentifier: "unwindSegue", sender: self)
+        }
     }
-    
-
-
 }
-
